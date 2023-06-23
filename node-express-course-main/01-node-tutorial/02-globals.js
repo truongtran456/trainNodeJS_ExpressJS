b525b094a3d922764d739b5cd675f4e018868aaa
Quoc_Truong <98575368+truongtran456@users.noreply.github.com>
2023-06-23 22:49:21 +0700

    Update 02-globals.js

--- node-express-course-main/01-node-tutorial/02-globals.js
+++ node-express-course-main/01-node-tutorial/02-globals.js
@@ -1,11 +1,9 @@
 // GLOBALS  - NO WINDOW !!!!
-
 // __dirname  - path to current directory
 // __filename - file name
 // require    - function to use modules (CommonJS)
 // module     - info about current module (file)
 // process    - info about env where the program is being executed
-
 // in ra 1000 lần
 console.log(__dirname)
 setInterval(() => {
